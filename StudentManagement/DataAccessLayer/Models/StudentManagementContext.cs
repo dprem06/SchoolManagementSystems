7e48bb43a226cb96a2c77849d6b64490c9a0de41
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-29 11:50:57 +0530

    cqrs pattern setuped

---
+++ StudentManagement/DataAccessLayer/Models/StudentManagementContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+namespace DataAccessLayer.Models
+{
+    public partial class StudentManagementContext : DbContext
+    {
+        public StudentManagementContext()
+        {
+        }
+
+        public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.ToTable("Course");
+
+                entity.HasAnnotation("Relational:IsTableExcludedFromMigrations", false);
+
+                entity.Property(e => e.CourseDescription)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CourseName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedOn)
+                    .HasColumnType("date")
+                    .HasAnnotation("Relational:ColumnType", "date");
+
+                entity.Property(e => e.IsActive)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- StudentManagement/StudentManagement/Startup.cs
+++ StudentManagement/StudentManagement/Startup.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using BuisnesServices.Repository;
+using DataAccessLayer.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace StudentManagement
 {
@@ -25,12 +32,91 @@ namespace StudentManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+            {
+                builder.WithOrigins("localhost:44300").AllowAnyMethod().AllowAnyHeader();
+            }));
+
+            // Database Configuration
+            var connectionString = Configuration.GetConnectionString("StudentManagementDatabase");
+            services.AddDbContext<StudentManagementContext>(options => options.UseSqlServer(connectionString));
+
+            // Auth0 Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dev-o56-7t2n.us.auth0.com/";
+                options.Audience = "https://employeeabb.com";
+            });
+
+            // Service Instance Initialise
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+            // Mediator config
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
             services.AddControllers();
+
+            // Swagger configuration
+            SwaggerConfigure(services);
+        }
+        private static void SwaggerConfigure(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Student Management API",
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+
+                            },
+                            new List<string>()
+                        }
+                    });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student Management V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
